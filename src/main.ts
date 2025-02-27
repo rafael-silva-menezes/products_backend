148d62e17c5c9c204251a66c2f2c1df37338fff9
Rafael Silva Menezes <rmenezes@questrade.com>
2025-02-27 20:33:33 -0300

    feat: configure CORS and port settings using ConfigService in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: configService.get('CORS_ORIGIN') || 'http://localhost:3000',
   });
-  await app.listen(process.env.PORT ?? 8000);
+
+  const port = configService.get('PORT') || 8000;
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();

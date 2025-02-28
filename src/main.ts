537cae07f7ce84617ffc0f3dc99e05d184481d73
Rafael Silva Menezes <rmenezes@questrade.com>
2025-02-28 06:16:58 -0300

    feat: integrate Redis caching in ProductsModule and update logging in main application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
 
   const port = configService.get('PORT') || 8000;
   await app.listen(port);
-  console.log(`Application is running on port ${port}`);
+  Logger.log(
+    `Application is running on port ${port}`,
+    app.getHttpServer().constructor.name,
+  );
 }
 bootstrap();

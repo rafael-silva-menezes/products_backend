6a4f55c6b3ee99a17d6de88d1020aec76e941b8d
Rafael Silva Menezes <rmenezes@questrade.com>
2025-03-02 00:33:26 -0300

    refactor: disable no-unsafe-member-access rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,6 +34,7 @@ export default tseslint.config(
       "@typescript-eslint/unbound-method": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
 
       '@typescript-eslint/interface-name-prefix': 'off',
       '@typescript-eslint/explicit-function-return-type': 'off',
